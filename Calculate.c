05f93bc8a2c22a54e8f595c5361888ce9375cf05
Robert <rassawyer@gmail.com>
2018-04-12 00:46:23 -0400

    Added the first code file. More to come!!

---
+++ Calculate.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+# include <string.h>
+
+int main ()
+{
+    int A = 1;
+    printf ("Hello, let's see if this works, shall we?\n ");
+    return 0;
+}
