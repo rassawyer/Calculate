b996e2362b7d5db731ea934b10d7810b5d93182c
Robert <rassawyer@gmail.com>
2019-10-14 17:53:27 -0400

    fixed indetarion that I couldn't easily do from my phone

--- Calculate.c
+++ Calculate.c
@@ -9,23 +9,23 @@ int main ()
     char op;
     printf ("Hello, let's see if this works, shall we?\n ");
     do{
-switch (op)
-{
-case '+'
-void add;
-break;
-case '-'
-void sub;
-break;
-case '*'
-void mul;
-break;
-case '/'
-void div;
-break;
-default
-printf "did not recognize that input; try again.";
-break;
-while (op != '=');
-return 0;
-}
+        switch (op)
+        {
+            case '+'
+                void add;
+            break;
+            case '-'
+                void sub;
+            break;
+            case '*'
+                void mul;
+            break;
+            case '/'
+                void div;
+            break;
+            default
+                printf "did not recognize that input; try again.";
+            break;
+            while (op != '=');
+            return 0;
+        }
