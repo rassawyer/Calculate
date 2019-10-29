8f36679c9ba3c4265195a8e55ed7cf45a312289c
Robert <rassawyer@gmail.com>
2019-10-28 20:39:22 -0400

    removed operations from main file

--- Calculate.c
+++ Calculate.c
@@ -57,39 +57,3 @@ void add ()
         scanf("%c", &(input.op));
     }while (input.op == '+');
 }
-
-void sub ()
-{
-    /* Code will go here. */
-    do{
-        printf ("%d -\n", input.first);
-        scanf("%d", &(input.sec));
-        input.first = input.first - input.sec;
-        scanf("%c", &(input.op));
-    }while (input.op == '-');
-    printf ("Subtract.\n");
-}
-
-void mul ()
-{
-    /* Code will go here. */
-    do{
-        printf ("%d *\n", input.first);
-        scanf("%d", &(input.sec));
-        input.first = input.first * input.sec;
-        scanf("%c", &(input.op));
-    }while (input.op == '*');
-    printf ("Multiply.\n");
-}
-
-void divide ()
-{
-    /* Code will go here. */
-    do{
-        printf ("%d /\n", input.first);
-        scanf("%d", &(input.sec));
-        input.first = input.first / input.sec;
-        scanf("%c", &(input.op));
-    }while (input.op == '/');
-    printf ("Divide.\n");
-}
