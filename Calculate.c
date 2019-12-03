57483a098d6cf06f7660d8655d716dad3c63dd38
Robert <rassawyer>
2019-12-03 01:32:01 +0000

    minor fixes, minor code rework, added todo.

--- Calculate.c
+++ Calculate.c
@@ -10,36 +10,33 @@ struct input input;
 
 int main ()
 {
-    printf ("Hello, let's see if this works, shall we?\n ");
-    do{
-        scanf("%d", &(input.first));
-        printf("\n");
-        while (getchar () !='\n' );
-        scanf("%c", &(input.op));
-        printf ("entered %d and %c\n", input.first, input.op);
-        switch (input.op)
-        {
-            case '+':
-    //            input.op = 0;
-                add();
-                break;
-            case '-':
-                sub();
-                break;
-            case '*':
-                mul();
-                break;
-            case '/':
-                divide();
-                break;
-            case '=':
-                break;
-                /*            default:
-                              printf ("did not recognize that input; try again.");
-                              break;
-                              */
-        }
-    }while (input.op != '=');
-    printf ("Result: %d \n", input.first);
-    return 0;
+	printf ("Hello, let's see if this works, shall we?\n ");
+	scanf("%d", &(input.first));
+	printf(" number 1 \n");
+	while (getchar () !='\n' );
+	scanf("%c", &(input.op));
+	printf ("entered %d and %c\n", input.first, input.op);
+	/* add sanity check here to prevent not operators from being stored in input.op. */
+	do{
+		switch (input.op)
+		{
+			case '+':
+				//            input.op = 0;
+				add();
+				break;
+			case '-':
+				sub();
+				break;
+			case '*':
+				mul();
+				break;
+			case '/':
+				divide();
+				break;
+		/*	case '=':
+				break;*/
+		}
+	}while (input.op != '=');
+	printf ("Result: %d \n", input.first);
+	return 0;
 }
